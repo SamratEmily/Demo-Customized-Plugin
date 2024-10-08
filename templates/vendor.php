5635c7a3b80ff322b8cb2116e8d5ce70edffba44
samrathossen2012 <samrat@welabs.dev>
2024-10-09 00:19:20 +0600

    Updated template

--- templates/vendor.php
+++ templates/vendor.php
@@ -1,9 +1,10 @@
 <div class="dokan-form-group">
-            <label for="company_name" class="dokan-w3 dokan-control-label">Company Name</label>
+    <label for="company_name" class="dokan-w3 dokan-control-label">Company Name</label>
 
-            <div class="dokan-w5 dokan-text-left">
-                <input type="text" id="company_name" name="company_name" class="dokan-form-control" 
-                        value="<?php echo esc_attr( $company_name ); ?>" 
-                        placeholder="Enter the company name">            
-            </div>
-        </div>
+    <div class="dokan-w5 dokan-text-left">
+        <input type="text" id="company_name" name="company_name" class="dokan-form-control" 
+                value="<?php echo esc_attr( $company_name ); ?>" 
+                placeholder="Enter the company name">            
+    </div>
+
+</div>
